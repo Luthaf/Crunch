aadfab3581f5ee257f02953709994e1750a8b16f
Luthaf <luthaf@luthaf.fr>
2014-06-13 20:18:12 +0100

    Better physics

--- game.lua
+++ game.lua
@@ -18,9 +18,11 @@ Main game state.
 
 local map, collision
 local tile_size = 70
-local g = 100 -- gravity
+local g = 800 -- gravity
 local X, Y -- player position
 local VX, VY -- player speed
+local MAX_VY = 750
+local MAX_VX = 200
 X, Y = 0, 0
 VX, VY = 0,0
 
@@ -28,11 +30,23 @@ local mario, mario_img
 
 function collide(dt, shape_one, shape_two, dx, dy)
     if shape_one == mario then
-        if dx ~= 0 then VX = 0 end
-        if dy ~= 0 then VY = 0 end
+        if dx ~= 0 then
+            VX = 0
+            X = X + dx
+        end
+        if dy ~= 0 then
+            VY = 0
+            Y = Y + dy
+        end
     elseif shape_two == mario then
-        if dx ~= 0 then VX = 0 end
-        if dy ~= 0 then VY = 0 end
+        if dx ~= 0 then
+            VX = 0
+            X = X - dx
+        end
+        if dy ~= 0 then
+            VY = 0
+            Y = Y - dy
+        end
     end
 end
 
@@ -62,17 +76,19 @@ function game:update(dt)
     X = X + VX*dt
     Y = Y + VY*dt
     VY = VY + g*dt
+    if VY > MAX_VY then VY = MAX_VY end
+    if VX > MAX_VX then VX = MAX_VX end
 end
 
 function game:keypressed(key, code)
     if key == 'escape' then
         gamestate.switch(menu)
     elseif key == 'f' then
-        VY = VY - 100
+        VY = VY - 300
     elseif key == 'left' then
-        VX = VX - 40
+        VX = VX - 100
     elseif key == 'right' then
-        VX = VX + 40
+        VX = VX + 100
    end
 end
 
