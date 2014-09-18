8397c4e53046f6ba945da6906185a0f783a92b18
Luthaf <luthaf@luthaf.fr>
2014-09-18 15:31:31 +0200

    Table serialisation functions, for future user save/load

--- libs/utils/tables.lua
+++ libs/utils/tables.lua
@@ -13,13 +13,13 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-Functions for table management
+Functions for tables management
 ]]
 
-tables = tables or {}
+table = table or {}
 
 -- Check if a table contains a given element in its values
-function tables.vcontains(table, element)
+function table.vcontains(table, element)
     for _, value in pairs(table) do
         if value == element then
             return true
@@ -29,7 +29,7 @@ function tables.vcontains(table, element)
 end
 
 -- Check if a table contains a given element in its keys
-function tables.kcontains(table, element)
+function table.kcontains(table, element)
     for key, _ in pairs(table) do
         if key == element then
             return true
@@ -39,7 +39,7 @@ function tables.kcontains(table, element)
 end
 
 -- Check if a table contains a given element either in key or in values
-function tables.contains(table, element)
+function table.contains(table, element)
     for key, value in pairs(table) do
         if value == element or key == element then
             return true
@@ -48,4 +48,111 @@ function tables.contains(table, element)
     return false
 end
 
-return tables
+--[[
+Table serialisation and write to a file
+from http://lua-users.org/wiki/SaveTableToFile
+]]--
+
+--// exportstring( string )
+--// returns a "Lua" portable version of the string
+local function exportstring( s )
+  return string.format("%q", s)
+end
+
+--// The Save Function
+function table.save(  tbl,filename )
+  local charS,charE = "   ","\n"
+  local file,err = io.open( filename, "wb" )
+  if err then return err end
+
+  -- initiate variables for save procedure
+  local tables,lookup = { tbl },{ [tbl] = 1 }
+  file:write( "return {"..charE )
+
+  for idx,t in ipairs( tables ) do
+     file:write( "-- Table: {"..idx.."}"..charE )
+     file:write( "{"..charE )
+     local thandled = {}
+
+     for i,v in ipairs( t ) do
+        thandled[i] = true
+        local stype = type( v )
+        -- only handle value
+        if stype == "table" then
+           if not lookup[v] then
+              table.insert( tables, v )
+              lookup[v] = #tables
+           end
+           file:write( charS.."{"..lookup[v].."},"..charE )
+        elseif stype == "string" then
+           file:write(  charS..exportstring( v )..","..charE )
+        elseif stype == "number" then
+           file:write(  charS..tostring( v )..","..charE )
+        end
+     end
+
+     for i,v in pairs( t ) do
+        -- escape handled values
+        if (not thandled[i]) then
+        
+           local str = ""
+           local stype = type( i )
+           -- handle index
+           if stype == "table" then
+              if not lookup[i] then
+                 table.insert( tables,i )
+                 lookup[i] = #tables
+              end
+              str = charS.."[{"..lookup[i].."}]="
+           elseif stype == "string" then
+              str = charS.."["..exportstring( i ).."]="
+           elseif stype == "number" then
+              str = charS.."["..tostring( i ).."]="
+           end
+        
+           if str ~= "" then
+              stype = type( v )
+              -- handle value
+              if stype == "table" then
+                 if not lookup[v] then
+                    table.insert( tables,v )
+                    lookup[v] = #tables
+                 end
+                 file:write( str.."{"..lookup[v].."},"..charE )
+              elseif stype == "string" then
+                 file:write( str..exportstring( v )..","..charE )
+              elseif stype == "number" then
+                 file:write( str..tostring( v )..","..charE )
+              end
+           end
+        end
+     end
+     file:write( "},"..charE )
+  end
+  file:write( "}" )
+  file:close()
+end
+
+function table.load( sfile )
+  local ftables,err = loadfile( sfile )
+  if err then return _,err end
+  local tables = ftables()
+  for idx = 1,#tables do
+     local tolinki = {}
+     for i,v in pairs( tables[idx] ) do
+        if type( v ) == "table" then
+           tables[idx][i] = tables[v[1]]
+        end
+        if type( i ) == "table" and tables[i[1]] then
+           table.insert( tolinki,{ i,tables[i[1]] } )
+        end
+     end
+     -- link indices
+     for _,v in ipairs( tolinki ) do
+        tables[idx][v[2]],tables[idx][v[1]] =  tables[idx][v[1]],nil
+     end
+  end
+  return tables[1]
+end
+
+return table
